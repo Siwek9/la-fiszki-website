b31d228979d187288ba97b952222369eb83fbae5
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2024-11-13 20:09:22 +0100

    Move project to ts + vite

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <ImportDialog v-if="importDialogOpen" />
+  <!-- <ImportDialog v-if="importDialogOpen" /> -->
   <h1 class="center-text">{{ name }}</h1>
   <h3>by: {{ author }}</h3>
   <AutoSaveButton
     @autoSave="toggleAutoSave"
-    ref="AutoSave"
+    ref="auto_save"
     :isAutoSaveOn="autoSave"
   />
   <ImportButton />
@@ -31,8 +31,8 @@
     />
   </div>
   <NewCardboard
-    v-for="cardboard in cardboards"
-    ref="cardboardArrayObject"
+    v-for="cardboard in flashcards"
+    ref="flashcard_array_object"
     @deleteCardboard="deleteCardboard"
     @deleteFrontField="deleteFrontField"
     @deleteBackField="deleteBackField"
@@ -48,148 +48,156 @@
     :name="name"
     :author="author"
     :side-name="sideName"
-    :cardboards="cardboards"
-    ref="Export"
+    :cardboards="flashcards"
+    ref="export"
   />
 </template>
 
-<script>
+<script lang="ts">
+import {defineComponent, useTemplateRef} from 'vue';
+
 import TextInput from './components/TextInput.vue';
 import NewCardboard from './components/NewCardboard.vue';
 import AddButton from './components/AddButton.vue';
 import ExportButton from './components/ExportButton.vue';
 import AutoSaveButton from './components/AutoSaveButton.vue';
-import ImportButton from './components/ImportButton.vue';
-import ImportDialog from './components/ImportDialog.vue';
+// import ImportButton from './components/ImportButton.vue';
+// import ImportDialog from './components/ImportDialog.vue';
+import Flashcard from './utils/Flashcard';
 
-export default {
+// type AutoSaveButtonType = InstanceType<typeof AutoSaveButton>;
+// type ExportButtonType = InstanceType<typeof ExportButton>;
+
+const autoSaveRef = useTemplateRef<typeof AutoSaveButton>('auto_save');
+const exportRef = useTemplateRef<typeof ExportButton>('export');
+
+export default defineComponent({
   name: 'App',
   data() {
     return {
-      importDialogOpen: false,
+      // importDialogOpen: false,
       autoSave: false,
-      name: 'New Cardboard',
+      name: 'New Flashcards Set',
       author: 'Unknown',
       sideName: {
         front: 'Default',
         back: 'Default',
       },
-      cardboards: [
-        {
-          id: 0,
-          front: [''],
-          back: [''],
-        },
-      ],
+      flashcards: [] as Array<Flashcard>,
     };
   },
   methods: {
-    deleteFrontField: function (cardboardID, fieldID) {
-      if (this.cardboards[cardboardID].front.length <= 1) return;
-      this.cardboards[cardboardID].front.splice(fieldID, 1);
+    deleteFrontField: function (flashcardID: number, fieldID: number) {
+      if (this.flashcards[flashcardID].front.length <= 1) return;
+      this.flashcards[flashcardID].front.splice(fieldID, 1);
     },
-    deleteBackField: function (cardboardID, fieldID) {
-      if (this.cardboards[cardboardID].back.length <= 1) return;
-      this.cardboards[cardboardID].back.splice(fieldID, 1);
+    deleteBackField: function (cardboardID: number, fieldID: number) {
+      if (this.flashcards[cardboardID].back.length <= 1) return;
+      this.flashcards[cardboardID].back.splice(fieldID, 1);
     },
-    toggleAutoSave: function (e) {
-      this.autoSave = e;
+    toggleAutoSave: function (toggle: boolean) {
+      this.autoSave = toggle;
       if (this.autoSave) {
-        this.$refs['AutoSave'].setCookie(
+        autoSaveRef.value?.setCookie(
           'last_save',
           JSON.stringify(
-            this.$refs['Export'].createFlashcardObject(this.name, this.author, this.sideName, this.cardboards)
+            exportRef.value?.createFlashcardObject(this.name, this.author, this.sideName, this.flashcards)
           ),
           7
         );
       } else {
-        this.$refs['AutoSave'].deleteCookie('last_save');
+        autoSaveRef.value?.methods?.deleteCookie('last_save');
       }
     },
     createNewCardboard: function () {
-      this.cardboards[this.cardboards.length] = {
-        id: this.cardboards.length,
+      this.flashcards[this.flashcards.length] = {
+        id: this.flashcards.length,
         front: [''],
         back: [''],
       };
-      if (this.autoSave) {
-        this.$refs['AutoSave'].setCookie(
-          'last_save',
-          JSON.stringify(
-            this.$refs['Export'].createFlashcardObject(this.name, this.author, this.sideName, this.cardboards)
-          ),
-          7
-        );
-      }
+      // if (this.autoSave) {
+      //     this.$refs['auto_save'].setCookie(
+      //         'last_save',
+      //         JSON.stringify(
+      //             this.$refs['export'].createFlashcardObject(
+      //                 this.name,
+      //                 this.author,
+      //                 this.sideName,
+      //                 this.flashcards
+      //             )
+      //         ),
+      //         7
+      //     );
+      // }
     },
-    moveToNextCardboard: function (id) {
-      if (id < this.cardboards.length - 1) {
-        this.$refs['cardboardArrayObject'][id + 1].$refs['front'].focus();
+    moveToNextCardboard: function (id: number) {
+      if (id < this.flashcards.length - 1) {
+        // this.$refs['cardboardArrayObject'][id + 1].$refs['front'].focus();
       } else {
         this.createNewCardboard();
       }
     },
-    deleteCardboard: function (id) {
-      if (this.cardboards.length <= 1) return;
-      this.cardboards.splice(id, 1);
+    deleteCardboard: function (id: number) {
+      if (this.flashcards.length <= 1) return;
+      this.flashcards.splice(id, 1);
       var i = 0;
-      this.cardboards.forEach(cardboard => {
+      this.flashcards.forEach(cardboard => {
         cardboard.id = i;
         i++;
       });
     },
-    setAuthor: function (author) {
+    setAuthor: function (author: string) {
       this.author = author;
     },
-    setName: function (name) {
+    setName: function (name: string) {
       this.name = name;
     },
-    setFrontSide: function (frontSide) {
+    setFrontSide: function (frontSide: string) {
       this.sideName.front = frontSide;
     },
-    setBackSide: function (backSide) {
+    setBackSide: function (backSide: string) {
       this.sideName.back = backSide;
     },
-    inputFrontChanged: function (value, id) {
+    inputFrontChanged: function (value: string, id: number) {
       if (value.slice(-1) == '/' && value.length > 1) {
-        this.cardboards[id].front.push('');
+        this.flashcards[id].front.push('');
       } else if (value.slice(-1) != '/') {
-        let lastIndex = this.cardboards[id].front.length - 1;
-        this.cardboards[id].front[lastIndex] = value;
+        let lastIndex = this.flashcards[id].front.length - 1;
+        this.flashcards[id].front[lastIndex] = value;
       }
     },
-    inputBackChanged: function (value, id) {
+    inputBackChanged: function (value: string, id: number) {
       if (value.slice(-1) == '/' && value.length > 1) {
-        this.cardboards[id].back.push('');
+        this.flashcards[id].back.push('');
       } else if (value.slice(-1) != '/') {
-        let lastIndex = this.cardboards[id].back.length - 1;
-        this.cardboards[id].back[lastIndex] = value;
+        let lastIndex = this.flashcards[id].back.length - 1;
+        this.flashcards[id].back[lastIndex] = value;
       }
     },
   },
   mounted() {
-    var data;
-    if ((data = this.$refs['AutoSave'].getCookie('last_save')) != null) {
-      this.autoSave = true;
-      data = JSON.parse(data);
-      if (data.name != undefined) this.name = data.name;
-      if (data.author != undefined) this.author = data.author;
-      if (data.sideName != undefined) {
-        if (data.sideName.front != undefined) this.sideName.front = data.sideName.front;
-        if (data.sideName.back != undefined) this.sideName.back = data.sideName.back;
-      }
-      if (data.cardboards != undefined) {
-        if (Array.isArray(data.cardboards)) {
-          data.cardboards.forEach((value, index) => {
-            this.cardboards[index] = {
-              id: index,
-              front: value.front != undefined ? value.front : '',
-              back: value.back != undefined ? value.back : '',
-            };
-          });
-        }
-      }
-    }
+    // var data;
+    // if ((data = this.$refs['auto_save'].getCookie('last_save')) != null) {
+    //     this.autoSave = true;
+    //     data = JSON.parse(data);
+    //     if (data.name != undefined) this.name = data.name;
+    //     if (data.author != undefined) this.author = data.author;
+    //     if (data.sideName != undefined) {
+    //         if (data.sideName.front != undefined) this.sideName.front = data.sideName.front;
+    //         if (data.sideName.back != undefined) this.sideName.back = data.sideName.back;
+    //     }
+    //     if (data.cardboards != undefined) {
+    //         if (Array.isArray(data.cardboards)) {
+    //             data.cardboards.forEach((value, index) => {
+    //                 this.flashcards[index] = {
+    //                     id: index,
+    //                     front: value.front != undefined ? value.front : '',
+    //                     back: value.back != undefined ? value.back : '',
+    //                 };
+    //             });
+    //         }
+    //     }
+    // }
   },
   components: {
     NewCardboard,
@@ -197,10 +205,10 @@ export default {
     ExportButton,
     TextInput,
     AutoSaveButton,
-    ImportButton,
-    ImportDialog,
+    // ImportButton,
+    // ImportDialog,
   },
-};
+});
 </script>
 
 <style>
